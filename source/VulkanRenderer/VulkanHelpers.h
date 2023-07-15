8160b38eacff641e02a77ed3772857553f60927a
DaanD <75164311+DaanDemaecker@users.noreply.github.com>
2023-07-15 12:36:14 +0200

    Added time manager

--- source/VulkanRenderer/VulkanHelpers.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace D3D
-{
-	void CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
-
-	void CopyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
-}
