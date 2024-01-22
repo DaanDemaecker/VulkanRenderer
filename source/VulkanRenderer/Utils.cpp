20b7305ec6994b006cfb1183fcc75fd39eae3e20
Daan Demaecker <daan.demaecker@gmail.com>
2024-01-22 15:36:36 +0100

    Added commentary to the Utils.h and Utils.cpp files

--- source/VulkanRenderer/Utils.cpp
+++ source/VulkanRenderer/Utils.cpp
@@ -1,133 +1,142 @@
+// File includes
 #include "Utils.h"
-#include <iostream>
-
 #include "TinyObjLoaderIncludes.h"
 
-std::vector<char> Utils::readFile(const std::string& filename);
-
-void Utils::LoadModel(const std::string& filename, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
-
-float Cross(const glm::vec2& v1, const glm::vec2& v2);
-
-glm::vec3 Reject(const glm::vec3& v1, const glm::vec3& v2);
-
-
 std::vector<char> Utils::readFile(const std::string& filename)
-
 {
-	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+	// Open file
+	std::ifstream file{ filename, std::ios::ate | std::ios::binary };
 
+	// If file failed to open, throw runtime error
 	if (!file.is_open())
 	{
 		throw std::runtime_error("Failed to open file " + filename);
 	}
 
-	//Because of the std::ios::ate flag, the file will be read from the end, getting the current position in the file will therefore give its size
+	// Because of the std::ios::ate flag, the file will be read from the end, getting the current position in the file will therefore give its size
 	size_t fileSize = static_cast<size_t>(file.tellg());
+
+	// Char buffer created with size of the file
 	std::vector<char> buffer(fileSize);
 
-	//Now that we have the size of the file, we can move back to the start and store the data into the buffer
+	// Now that we have the size of the file, we can move back to the start and store the data into the buffer
 	file.seekg(0);
 	file.read(buffer.data(), fileSize);
 
+	// Close the file
 	file.close();
 
+	// Return the buffer
 	return buffer;
 }
 
 void Utils::LoadModel(const std::string& filename, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices)
 {
+	// Clear the vectors in case they aren't empty
 	vertices.clear();
 	indices.clear();
 
+	// Create needed objects to read in .obj file
 	tinyobj::attrib_t attrib{};
 	std::vector<tinyobj::shape_t> shapes{};
 	std::vector<tinyobj::material_t> materials{};
 
+	// Create objects for error throwing
 	std::string warn, err;
 
+	// Read file, returned false, throw error
 	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str()))
 	{
 		throw std::runtime_error(warn + err);
 	}
 
+	// Create map to store vertices
 	std::unordered_map<Vertex, uint32_t> uniqueVertices{};
 
+	// Loop trough every shape that was read from the file
 	for (const auto& shape : shapes)
 	{
+		// Loop trough all indices in current shape
 		for (const auto& index : shape.mesh.indices)
 		{
+			// Create empty vertex
 			Vertex vertex{};
 
+			// Add position to vertex
 			vertex.pos = {
 				attrib.vertices[static_cast<uint64_t>(3) * index.vertex_index],
 				attrib.vertices[static_cast<uint64_t>(3) * index.vertex_index + static_cast<uint64_t>(1)],
 				attrib.vertices[static_cast<uint64_t>(3) * index.vertex_index + static_cast<uint64_t>(2)]
 			};
 
+			// Add UV coords to vertex
 			vertex.texCoord = {
 				attrib.texcoords[static_cast<uint64_t>(2) * index.texcoord_index],
 				1.0f - attrib.texcoords[static_cast<uint64_t>(2) * index.texcoord_index + 1]
 			};
 
+			// Add normal to vertex
 			vertex.normal = {
 				attrib.normals[static_cast<uint64_t>(3) * index.normal_index],
 				attrib.normals[static_cast<uint64_t>(3) * index.normal_index + static_cast<uint64_t>(1)],
 				attrib.normals[static_cast<uint64_t>(3) * index.normal_index + static_cast<uint64_t>(2)]
 			};
 
-			vertex.color = { 1.0f, 1.0f, 1.0f };
+			// Add color to vertex
+			vertex.color = {
+				attrib.colors[static_cast<uint64_t>(3) * index.vertex_index],
+				attrib.colors[static_cast<uint64_t>(3) * index.vertex_index + static_cast<uint64_t>(1)],
+				attrib.colors[static_cast<uint64_t>(3) * index.vertex_index + static_cast<uint64_t>(2)]
+			};
 
+			// If vertex isn't in uniqueVertices vector, add it
 			if (uniqueVertices.count(vertex) == 0)
 			{
 				uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
 				vertices.push_back(vertex);
 			}
 
+			// Add index to indices vector
 			indices.push_back(uniqueVertices[vertex]);
 		}
 	}
 
+	// After all vertices are added loop trought them to calculate the tangents
 	for (size_t i = 0; i < indices.size(); i += 3)
 	{
+		// Get the the indices of the current triangle
 		uint32_t index0 = indices[i];
 		uint32_t index1 = indices[i + 1];
 		uint32_t index2 = indices[i + 2];
 
+		// Get the vertices asociated with this triangle
 		Vertex& v0 = vertices[index0];
 		Vertex& v1 = vertices[index1];
 		Vertex& v2 = vertices[index2];
 
+		// Get 2 edges of this triangle
 		glm::vec3 edge1 = v1.pos - v0.pos;
 		glm::vec3 edge2 = v2.pos - v0.pos;
 
+		// Get the difference in UV over these edges
 		glm::vec2 deltaUV1 = v1.texCoord - v0.texCoord;
 		glm::vec2 deltaUV2 = v2.texCoord - v0.texCoord;
 
+		// Calculate the scaling factor for normalizing the vector
 		float r = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV1.y * deltaUV2.x);
+
+		// Calculate the tangent
 		glm::vec3 tangent = (edge1 * deltaUV2.y - edge2 * deltaUV1.y) * r;
 
+		// Add the tangent to the 3 vertices
 		v0.tangent += tangent;
 		v1.tangent += tangent;
 		v2.tangent += tangent;
 	}
 
+	// Flip the Z attribute of all normals as we are using a left handed coordinate system and the obj uses right handed
 	for (auto& v : vertices)
 	{
-		v.tangent = glm::normalize(v.tangent);
-
 		v.normal.z *= -1.f;
-		//v.tangent.z *= -1.f;
 	}
-}
-
-float Cross(const glm::vec2& v1, const glm::vec2& v2)
-{
-	return v1.x * v2.y - v1.y * v2.x;
-}
-
-
-glm::vec3 Reject(const glm::vec3& v1, const glm::vec3& v2)
-{
-	return (v1 - v2 * (glm::dot(v1, v2) / glm::dot(v2, v2)));
 }
--- source/VulkanRenderer/Utils.h
+++ source/VulkanRenderer/Utils.h
@@ -1,17 +1,30 @@
+// Utils.h
+// This file defines utility functions with the Utils namespace
+// Currently these functions include reading in binary files and obj files
+
 #ifndef UtilsIncluded
 #define UtilsIncluded
 
+// File includes
 #include "Structs.h"
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+// Standard library includes
 #include <vector>
 #include <fstream>
 
 namespace Utils
 {
+	// Reads in a file and returns it as a char vector
+	// This function is mainly used to read in shaders
+	// Parameters:
+	//     - filename: The name of the file to be read
 	std::vector<char> readFile(const std::string& filename);
 
+	// Uses TinyObjLoader to store a .obj file in a vertex- and indexVector
+	// Parameters:
+	//     - filename: The name of the obj file
+	//     - vertices: The vector that will be used to store the vertices
+	//     - indices: The vector that will be used to store the indices
 	void LoadModel(const std::string& filename, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
 }
 
