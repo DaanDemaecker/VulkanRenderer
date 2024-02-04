6edcb4183a317715d7f3b0afc575c82dd73310d7
Daan Demaecker <daan.demaecker@gmail.com>
2024-02-03 18:35:15 -0800

    Added comments to material class

--- source/VulkanRenderer/Material.cpp
+++ source/VulkanRenderer/Material.cpp
@@ -1,42 +1,57 @@
+// Material.cpp
+
+// File includes
 #include "Material.h"
 #include "VulkanRenderer.h"
 #include "Utils.h"
-
 #include "DescriptorPoolWrapper.h"
 #include "DescriptorPoolManager.h"
-
 #include "STBIncludes.h"
 
 D3D::Material::Material(const std::string& pipelineName)
 {
+	// Get the requested pipeline from the renderer
 	m_PipelinePair = VulkanRenderer::GetInstance().GetPipeline(pipelineName);
 }
 
 void D3D::Material::CreateDescriptorSets(Model* pModel, std::vector<VkDescriptorSet>& descriptorSets)
 {
+	// Get pointer to the descriptorpool wrapper
 	auto descriptorPool = GetDescriptorPool();
+	// Add model to descriptorpool wrapper
 	descriptorPool->AddModel(pModel);
+	// Create descriptorpools
 	descriptorPool->CreateDescriptorSets(GetDescriptorLayout(), descriptorSets);
 }
 
 void D3D::Material::UpdateDescriptorSets(std::vector<VkBuffer>& uboBuffers, std::vector<VkDescriptorSet>& descriptorSets)
 {
+	// Get pointer to the descriptorpool wrapper
 	auto descriptorPool = GetDescriptorPool();
+	// Create vector of vectors of vkBuffers
 	std::vector<std::vector<VkBuffer>> uboList{ uboBuffers, D3D::VulkanRenderer::GetInstance().GetLightBuffers()};
 
+	// Create vector for buffersizes
 	std::vector<VkDeviceSize> uboSizes(2);
+	// Set first size to size of UniformBufferObject
 	uboSizes[0] = sizeof(UniformBufferObject);
+	// Set second size to size of LightObject
 	uboSizes[1] = sizeof(LightObject);
 
+	// Update descriptorsets
 	descriptorPool->UpdateDescriptorSets(descriptorSets, uboList, uboSizes);
 }
 
 std::vector<VkDescriptorSetLayout>& D3D::Material::GetDescriptorLayout()
 {
+	// Get the descriptorsets layout
+	// Standard material has 1 vertshader ubo, 1 fragshader ubo and no textures
 	return VulkanRenderer::GetInstance().GetDescriptorSetLayout(1, 1, 0);
 }
 
 D3D::DescriptorPoolWrapper* D3D::Material::GetDescriptorPool()
 {
+	// Get descriptorpoolwrapper
+	// Standard material has 2 ubos and 0 textures
 	return D3D::VulkanRenderer::GetInstance().GetDescriptorPoolManager()->GetDescriptorPool(2, 0);
 }
--- source/VulkanRenderer/Material.h
+++ source/VulkanRenderer/Material.h
@@ -1,18 +1,31 @@
+// Material.h
+// This is the base class for all the materials
+// Materials are used to bind a graphics pipeline and/or a texture to a model
+
 #ifndef MaterialIncluded
 #define MaterialIncluded
 
+// File includes
 #include "Structs.h"
+
+// Standard library includes
 #include <iostream>
 
 namespace D3D
 {
+	// Class forward declaration for descriptorpoolwrapper
 	class DescriptorPoolWrapper;
+	// Class forward declaration for model
 	class Model;
 
 	class Material
 	{
 	public:
+		// Constructor
+		// Parameters:
+		//     pipelineName: the name of the graphicspipeline this material will use, standard set to "Default"
 		Material(const std::string& pipelineName = "Default");
+		// Default constructor
 		virtual ~Material() = default;
 
 		Material(Material& other) = delete;
@@ -21,17 +34,29 @@ namespace D3D
 		Material& operator=(Material& other) = delete;
 		Material& operator=(Material&& other) = delete; 
 
+		// Get the pipeline pair that is used by this material
 		PipelinePair& GetPipeline() { return m_PipelinePair; }
 
+		// Create the descriptorsets
+		// Parameters:
+		//     pModel: the model that the descriptorsets belong to
+		//     descriptorSets: vector of descriptorsets that have to be created
 		virtual void CreateDescriptorSets(Model* pModel, std::vector<VkDescriptorSet>& descriptorSets);
 
+		// Update the descriptorsets
+		// Parameters:
+		//     uboBuffers: vector of the buffers for Uniform Buffer Objects
+		//     descriptorsets: the descriptorsets that should be updated
 		virtual void UpdateDescriptorSets(std::vector<VkBuffer>& uboBuffers, std::vector<VkDescriptorSet>& descriptorSets);
 
+		// Get the descriptor set layout
 		virtual std::vector<VkDescriptorSetLayout>& GetDescriptorLayout();
 
+		// Get the descriptorpool wrapper
 		virtual DescriptorPoolWrapper* GetDescriptorPool();
 
 	protected:
+		// The pipeline pair that is used for this material
 		PipelinePair m_PipelinePair{};
 	};
 }
