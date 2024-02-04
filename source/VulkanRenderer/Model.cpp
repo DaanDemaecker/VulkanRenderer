6b430fddf57c9de8bfee60b72fd77bc305424358
Daan Demaecker <daan.demaecker@gmail.com>
2024-02-03 21:48:53 -0800

    Added comentary to Model class

--- source/VulkanRenderer/Model.cpp
+++ source/VulkanRenderer/Model.cpp
@@ -1,3 +1,6 @@
+// Model.cpp
+
+// File includes
 #include "Model.h"
 #include "VulkanRenderer.h"
 #include "Utils.h"
@@ -5,15 +8,18 @@
 #include "TimeManager.h"
 #include "DescriptorPoolWrapper.h"
 
+// Standard library includes
 #include <memory>
 
 D3D::Model::Model()
 {
+	// Crate default material as placeholder
 	m_pMaterial = std::make_shared<D3D::Material>();
 }
 
 D3D::Model::~Model()
 {
+	// Check if model is initialized, if it is, clean it up
 	if (m_Initialized)
 	{
 		Cleanup();
@@ -22,178 +28,252 @@ D3D::Model::~Model()
 
 void D3D::Model::LoadModel(const std::string& textPath)
 {
+	// Check if model is initialized, if it is, clean up first
 	if (m_Initialized)
 	{
 		m_Initialized = false;
 		Cleanup();
 	}
 
+	// Load model into vertex and index vectors
 	Utils::LoadModel(textPath, m_Vertices, m_Indices);
+	// Create vertex buffer
 	CreateVertexBuffer();
+	// Create index buffer
 	CreateIndexBuffer();
+	// Create uniform buffer
 	CreateUniformBuffers();
+	// Create descriptorsets
 	CreateDescriptorSets();
 
+	// Set initialized to true
 	m_Initialized = true;
 }
 
 void D3D::Model::SetMaterial(std::shared_ptr<Material> pMaterial)
 {
+	// Remove model from old descriptorpool
 	m_pMaterial->GetDescriptorPool()->RemoveModel(this);
+	// Set new material
 	m_pMaterial = pMaterial;
+	// Create new descriptorpool
 	CreateDescriptorSets();
 }
 
 void D3D::Model::Update()
 {
+	// Initialize rotation speed
 	constexpr float rotSpeed{-glm::radians(15.f)};
 
+	// Calculate amount of rotation
 	float rotAmount{rotSpeed * TimeManager::GetInstance().GetDeltaTime()};
 
+	// Set new rotation
 	SetRotation(m_Rotation.x, m_Rotation.y + rotAmount , m_Rotation.z);
 }
 
 void D3D::Model::Render()
 {
+	// If model isn't initialize, return
 	if (!m_Initialized)
 		return;
 
+	// Get reference to renderer
 	auto& renderer{ VulkanRenderer::GetInstance() };
-
+	// Get index of current frame
 	auto frame{ renderer.GetCurrentFrame() };
 
+	// Check if UBOs have changed, if so, update them
 	if (m_UboChanged[frame])
 	{
 		UpdateUniformBuffer(frame);
 	}
 
+	// Render model
 	renderer.Render(this, renderer.GetCurrentCommandBuffer(), &m_DescriptorSets[frame], GetPipeline());
 }
 
 void D3D::Model::SetPosition(float x, float y, float z)
 {
+	// Set new position
 	m_Position = { x, y, z };
+	// Set dirty flags
 	SetDirtyFlags();
 }
 
 void D3D::Model::SetRotation(float x, float y, float z)
 {
+	// Set new rotation
 	m_Rotation = { x, y, z };
+	// Set dirty flags
 	SetDirtyFlags();
 }
 
 void D3D::Model::SetScale(float x, float y, float z)
 {
+	// Set new scale
 	m_Scale = { x, y, z };
+	// Set dirty flags
 	SetDirtyFlags();
 }
 
 void D3D::Model::CreateVertexBuffer()
 {
+	// Get reference to renderer
 	auto& renderer{ VulkanRenderer::GetInstance() };
-	auto device = renderer.GetDevice();
+	// Get reference to logical device
+	auto& device = renderer.GetDevice();
 
-	VkDeviceSize bufferSize = sizeof(m_Vertices[0]) * m_Vertices.size();
+	// Calculate buffer size for vertices
+	VkDeviceSize bufferSize = sizeof(Vertex) * m_Vertices.size();
 
+	// Create staging buffer
 	VkBuffer stagingBuffer;
+	// Create staging buffer memory
 	VkDeviceMemory stagingBufferMemory;
 
+	// Create buffer
 	renderer.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
+	// Create void pointer for data
 	void* data;
 
+	// Map memory of data to stagingbuffermemory
 	vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+	// Copy data from vertices to data pointer
 	memcpy(data, m_Vertices.data(), static_cast<size_t>(bufferSize));
+	// Unmap memory
 	vkUnmapMemory(device, stagingBufferMemory);
 
+	// Create the buffer
 	renderer.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_VertexBuffer, m_VertexBufferMemory);
 
+	// Copy buffer
 	renderer.CopyBuffer(stagingBuffer, m_VertexBuffer, bufferSize);
 
+	// Destroy staging buffer
 	vkDestroyBuffer(device, stagingBuffer, nullptr);
+	// Free staging buffer memory
 	vkFreeMemory(device, stagingBufferMemory, nullptr);
 }
 
 void D3D::Model::CreateIndexBuffer()
 {
+	// Get reference to renderer
 	auto& renderer{ VulkanRenderer::GetInstance() };
-	auto device = renderer.GetDevice();
+	// Get reference to logical device
+	auto& device = renderer.GetDevice();
 
-	VkDeviceSize bufferSize = sizeof(m_Indices[0]) * m_Indices.size();
+	// Calculate buffer size for indices
+	VkDeviceSize bufferSize = sizeof(uint32_t) * m_Indices.size();
 
+	// Create staging buffer
 	VkBuffer stagingBuffer;
+	// Create staging buffer memory
 	VkDeviceMemory stagingBufferMemory;
+	// Create buffer
 	renderer.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
+	// Create void pointer for data
 	void* data;
+	// Map memory of data to staging buffer memory
 	vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+	// Copy memory from indices to data
 	memcpy(data, m_Indices.data(), static_cast<size_t>(bufferSize));
 
+	// Create buffer
 	renderer.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_IndexBuffer, m_IndexBufferMemory);
 
+	// Copy staging buffer to index buffer
 	renderer.CopyBuffer(stagingBuffer, m_IndexBuffer, bufferSize);
 
+	// Destroy staging buffer
 	vkDestroyBuffer(device, stagingBuffer, nullptr);
+
+	// Free staging buffer memory
 	vkFreeMemory(device, stagingBufferMemory, nullptr);
 }
 
 void D3D::Model::CreateUniformBuffers()
 {
+	// Get reference to renderer
 	auto& renderer = VulkanRenderer::GetInstance();
+	// Get amount of frames
+	auto frames = renderer.GetMaxFrames();
 
+	// Get size of Uniform Buffer Object
 	VkDeviceSize bufferSize = sizeof(UniformBufferObject);
 
-	m_UboBuffers.resize(renderer.GetMaxFrames());
-	m_UbosMemory.resize(renderer.GetMaxFrames());
-	m_UbosMapped.resize(renderer.GetMaxFrames());
+	// Resize ubobuffers to amount of frames
+	m_UboBuffers.resize(frames);
+	// Resize ubo memory to amount of frames
+	m_UbosMemory.resize(frames);
+	// Resize mapped memory to amount of frames
+	m_UbosMapped.resize(frames);
+	// Resize ubos to amount of frames
+	m_Ubos.resize(frames);
+	// Resize dirty flages to amount of frames
+	m_UboChanged.resize(frames);
 
-	m_Ubos.resize(renderer.GetMaxFrames());
-	m_UboChanged.resize(renderer.GetMaxFrames());
-
+	// Set dirty flag
 	SetDirtyFlags();
 
-
-	for (size_t i = 0; i < renderer.GetMaxFrames(); ++i)
+	// Loop for the amount of frames there are
+	for (size_t i = 0; i < frames; ++i)
 	{
+		// Create memory
 		renderer.CreateBuffer(bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
 			m_UboBuffers[i], m_UbosMemory[i]);
 
+		// Map memory from uboMemory to ubosmapped
 		vkMapMemory(renderer.GetDevice(), m_UbosMemory[i], 0, bufferSize, 0, &m_UbosMapped[i]);
 	}
 }
 
 void D3D::Model::CreateDescriptorSets()
 {
+	// Create descriptorsets
 	m_pMaterial->CreateDescriptorSets(this, m_DescriptorSets);
+	// Update descriptors
 	UpdateDescriptorSets();
 }
 
 void D3D::Model::UpdateDescriptorSets()
 {
+	// Update descriptorsets
 	m_pMaterial->UpdateDescriptorSets(m_UboBuffers, m_DescriptorSets);
 }
 
 void D3D::Model::UpdateUniformBuffer(uint32_t frame)
 {
+	// Get translation matrix
 	glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), m_Position);
-	glm::vec3 rotationAngles(m_Rotation.x, m_Rotation.y, m_Rotation.z);
 
+	// Convert rotation to quaternion
 	glm::quat quaternion = glm::quat(m_Rotation);
+	// Get rotation matrix
 	glm::mat4 rotationMatrix = glm::mat4_cast(quaternion);
 
+	// Get scaling matrix
 	glm::mat4 scalingMatrix = glm::scale(glm::mat4(1.0f), m_Scale);
 
+	// Set Ubo
 	m_Ubos[frame].model = translationMatrix * rotationMatrix * scalingMatrix;
 
+	// Update ubo
+	// Send to renderer to update camera matrix
 	VulkanRenderer::GetInstance().UpdateUniformBuffer(m_Ubos[frame]);
 
+	// Reset dirty flag
 	m_UboChanged[frame] = false;
 
+	// Copy memory from ubos to ubosmapped
 	memcpy(m_UbosMapped[frame], &m_Ubos[frame], sizeof(m_Ubos[frame]));
 }
 
 PipelinePair& D3D::Model::GetPipeline()
 {
+	// Check if material exist, if not, return default
 	if (m_pMaterial != nullptr)
 	{
 		return m_pMaterial->GetPipeline();
@@ -204,26 +284,36 @@ PipelinePair& D3D::Model::GetPipeline()
 
 void D3D::Model::Cleanup()
 {
+	// Get reference to renderer
 	auto& renderer = D3D::VulkanRenderer::GetInstance();
+	// Get reference to device
+	auto& device = renderer.GetDevice();
 
-	auto device = renderer.GetDevice();
-
+	// Wait until device is idle
 	vkDeviceWaitIdle(device);
 
+	// Destroy index buffer
 	vkDestroyBuffer(device, m_IndexBuffer, nullptr);
+	// Free index buffer memory
 	vkFreeMemory(device, m_IndexBufferMemory, nullptr);
 
+	// Destroy vertex buffer
 	vkDestroyBuffer(device, m_VertexBuffer, nullptr);
+	// Free vertex buffer
 	vkFreeMemory(device, m_VertexBufferMemory, nullptr);
 
+	// Loop for the amount of frames
 	for (size_t i = 0; i < renderer.GetMaxFrames(); ++i)
 	{
+		// Destroy uboBuffers
 		vkDestroyBuffer(device, m_UboBuffers[i], nullptr);
+		// Free ubo buffer memory
 		vkFreeMemory(device, m_UbosMemory[i], nullptr);
 	}
 }
 
 void D3D::Model::SetDirtyFlags()
 {
+	// Set all dirty flags
 	std::fill(m_UboChanged.begin(), m_UboChanged.end(), true);
 }
--- source/VulkanRenderer/Model.h
+++ source/VulkanRenderer/Model.h
@@ -1,21 +1,29 @@
+// Model.h
+// This classed is used to load in and render 3D models
+
 #ifndef ModelIncluded
 #define ModelIncluded
 
+// File includes
 #include "GLFWIncludes.h"
 #include "GLMIncludes.h"
 #include "Structs.h"
 
+// Standard library includes
 #include <memory>
 #include <iostream>
 
 namespace D3D
 {
+	// Class forward declaration for material
 	class Material;
 
 	class Model : public std::enable_shared_from_this<Model>
 	{
 	public:
+		// Constructor
 		Model();
+		// Destructor
 		~Model();
 
 		Model(Model& other) = delete;
@@ -24,71 +32,119 @@ namespace D3D
 		Model& operator=(Model& other) = delete;
 		Model& operator=(Model&& other) = delete;
 
+		// Load the model
+		// Parameters:
+		//     textPath: textpath to where the model is stored
 		void LoadModel(const std::string& textPath);
+		
+		// Set the material
+		// Parameters:
+		//     pMaterial: the material requested
 		void SetMaterial(std::shared_ptr<Material> pMaterial);
 
+		// Update model
 		void Update();
 
+		// Render model
 		void Render();
 
-		//Getters for rendering
+		// Get vertex buffer
 		const VkBuffer& GetVertexBuffer() const { return m_VertexBuffer; }
+		// Get index buffer
 		const VkBuffer& GetIndexBuffer() const { return m_IndexBuffer; }
+		// Get amound of indices
 		size_t GetIndexAmount() const { return m_Indices.size(); }
 
-		//Transform functions
+		// Set position
+		// Parameters:
+		//     x: x-position
+		//     y: y-position
+		//     z: z-position
 		void SetPosition(float x, float y, float z);
+
+		// Set rotation
+		// Parameters:
+		//     x: x-angle
+		//     y: y-angle
+		//     z: z-angle
 		void SetRotation(float x, float y, float z);
+
+		// Set scale
+		// Parameters:
+		//     x: x-scale
+		//     y: y-scale
+		//     z: z-scale
 		void SetScale(float x, float y, float z);
 
+		// Create the descriptorsets
 		void CreateDescriptorSets();
 
 	private:
-		//Member variables
+		//Is model initialized
 		bool m_Initialized{ false };
 
+		// Rotation vector
 		glm::vec3 m_Rotation{};
+		// Position vector
 		glm::vec3 m_Position{};
+		// Scale vector
 		glm::vec3 m_Scale{ 1, 1, 1 };
 
+		// Vector for Uniform Buffer Objects
 		std::vector<UniformBufferObject> m_Ubos{};
+		// Vector of dirty flags for UBOs
 		std::vector<bool> m_UboChanged{};
 
+		// Vector of buffers for UBOs
 		std::vector<VkBuffer> m_UboBuffers{};
+		// Vector for memories for UBOs
 		std::vector<VkDeviceMemory> m_UbosMemory{};
+		// Pointers to mapped UBOs
 		std::vector<void*> m_UbosMapped{};
 
+		// Vector of descriptorsets
 		std::vector<VkDescriptorSet> m_DescriptorSets{};
 
-
+		// Vector of vertices
 		std::vector<Vertex> m_Vertices{};
+		// Vector of indices
 		std::vector<uint32_t> m_Indices{};
 
-		//Vertex buffer
+		// Vertex buffer
 		VkBuffer m_VertexBuffer{};
+		// Vertex buffer memory
 		VkDeviceMemory m_VertexBufferMemory{};
 
-		//Index buffer
+		// Index buffer
 		VkBuffer m_IndexBuffer{};
+		// Index buffer memory
 		VkDeviceMemory m_IndexBufferMemory{};
 
 		//Material
 		std::shared_ptr<Material> m_pMaterial{};
 
-		//Initialization functions
+		// Create vertex buffer
 		void CreateVertexBuffer();
+		// Create index buffer
 		void CreateIndexBuffer();
+		// Create uniform buffer
 		void CreateUniformBuffers();
 
+		// UPdate descriptorsets
 		void UpdateDescriptorSets();
 
+		// Update uniform buffers
+		// Parameters:
+		//     frame: index of current frame
 		void UpdateUniformBuffer(uint32_t frame);
 
-		//Texture functions
+		// Get the pipeline that the material is bound to
 		PipelinePair& GetPipeline();
 
+		// CLeanup
 		void Cleanup();
 
+		// Set dirty flags for UBOs
 		void SetDirtyFlags();
 	};
 }
