19e745da47bc6efe6251a5c50880c07ac8e7dc4f
Daan Demaecker <daan.demaecker@gmail.com>
2024-02-06 09:46:58 +0100

    Added commentary to timemanger class

--- source/VulkanRenderer/TimeManager.cpp
+++ source/VulkanRenderer/TimeManager.cpp
@@ -1,7 +1,14 @@
+// TimeManager.cpp
+
+// file includes
 #include "TimeManager.h"
 
 void D3D::TimeManager::SetDeltaTime(float dt)
 {
+	// Set delta time
 	m_DeltaTime = dt;
+	// Calculate fps
 	m_Fps = static_cast<int>(1.0f / dt);
+	// Add to total time
+	m_TotalTime += dt;
 }
--- source/VulkanRenderer/TimeManager.h
+++ source/VulkanRenderer/TimeManager.h
@@ -1,6 +1,10 @@
+// TimeManager.h
+// This class will manage all things time related such as deltaTime, fps etc.
+
 #ifndef TimeManagerIncluded
 #define TimeManagerIncluded
 
+// File includes
 #include "Singleton.h"
 
 namespace D3D
@@ -8,18 +12,33 @@ namespace D3D
 	class TimeManager final : public Singleton<TimeManager>
 	{
 	public:
+		// Set the delta time for this frame
+		// Parameters:
+		//     dt: delta time in seconds
 		void SetDeltaTime(float dt);
+		// Get delta time for this frame
 		float GetDeltaTime() const { return m_DeltaTime; }
 
+		// Get current fps
 		int GetFps() const { return m_Fps; }
 
+		// Set delta time for fixed update
 		void SetFixedTime(float ft) { m_FixedTime = ft; }
+		// Get delta time for fixed update
 		float GetFixedTime() const { return m_FixedTime; }
 
+		// Get total time the app has been running
+		float GetTotalTime() const { return m_TotalTime; }
+
 	private:
+		// The current amount of frames per second
 		int m_Fps = 0;
+		// The delta time for the current frame
 		float m_DeltaTime = 0;
+		// The fixed time for the fix
 		float m_FixedTime = 0;
+		// Total time the app has been running
+		float m_TotalTime = 0;
 	};
 }
 
