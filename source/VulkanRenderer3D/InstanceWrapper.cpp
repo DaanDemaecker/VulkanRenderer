4074ef548290def9f2fd7113f8810932cee0dcee
Daan Demaecker <daan.demaecker@gmail.com>
2024-03-01 15:39:42 +0100

    3D: added comments to the InstanceWrapper class

--- source/VulkanRenderer3D/InstanceWrapper.cpp
+++ source/VulkanRenderer3D/InstanceWrapper.cpp
@@ -1,6 +1,10 @@
+// InstaceWrapper.cpp
+
+// File includes
 #include "InstanceWrapper.h"
 #include "GLFWIncludes.h"
 
+// Standard library includes
 #include <stdexcept>
 #include <iostream>
 
@@ -10,112 +14,161 @@ D3D::InstanceWrapper::InstanceWrapper(bool enableValidationLayers, const std::ve
 	//If in debug and validation layers are requested, set up validation layers
 	if (enableValidationLayers && !CheckValidationLayerSupport(validationLayers))
 	{
+		// If validation layers are not supported, throw runtime error
 		throw std::runtime_error("validation layers requested, but not available!");
 	}
 #endif
 
 	//provide usefull information to the driver to optimize application
-	VkApplicationInfo appInfo{};
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Vulkan Renderer";
-	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.pEngineName = "D3D";
-	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
+	VkApplicationInfo appInfo{};;
+	// Setup application info
+	SetupApplicationInfo(appInfo);
 
-	//tell Vulkan driver which extensions and validation layers are wanted
+	// Create instance create info
 	VkInstanceCreateInfo createInfo{};
+	// Set type to instance create info
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	// Give pointer to app info
 	createInfo.pApplicationInfo = &appInfo;
 
+	// Get required extensions
 	auto extensions = GetRequiredExtensions(enableValidationLayers);
+	// Set extension count to size of extensions
 	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	// Give data of extensions
 	createInfo.ppEnabledExtensionNames = extensions.data();
 
+	// Create debug create info
 	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
 	if (enableValidationLayers)
 	{
+		// If validation layers enabled:
+		// Set layercount to validation layers size
 		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		// Set enabled layers names to data of validation layers
 		createInfo.ppEnabledLayerNames = validationLayers.data();
 
+		// Pupulate the debug messenger create info
 		PopulateDebugMessengerCreateInfo(debugCreateInfo);
+		// Set pNext to a pointer to debug create info
 		createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
 	}
 	else
 	{
+		// If validation layers are disabled:
+		// Set layercount to 0
 		createInfo.enabledLayerCount = 0;
-
+		// Set pNext to nullptr
 		createInfo.pNext = nullptr;
 	}
 
-	VkResult result = vkCreateInstance(&createInfo, nullptr, &m_Instance);
-
-	if (result != VK_SUCCESS)
+	// Create vulkan instance
+	if (vkCreateInstance(&createInfo, nullptr, &m_Instance) != VK_SUCCESS)
 	{
+		// If unsuccessful, throw runtime error
 		throw std::runtime_error("failed to create instance");
 	}
 
+	// Set up debug messenger
 	SetupDebugMessenger(enableValidationLayers);
 }
 
 void D3D::InstanceWrapper::cleanup(bool enableValidationLayers)
 {
+	// If validation layers enabled, delete debug messenger
 	if (enableValidationLayers)
 	{
 		DestroyDebugUtilsMessegerEXT(m_Instance, m_DebugMessenger, nullptr);
 	}
-
+	
+	// Destroy instance
 	vkDestroyInstance(m_Instance, nullptr);
+}
+
+void D3D::InstanceWrapper::SetupApplicationInfo(VkApplicationInfo& appInfo)
+{
+	// Set type to application info
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	// Set name of application
+	appInfo.pApplicationName = "Vulkan Renderer";
+	// Set version of application
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	// Set name of engine
+	appInfo.pEngineName = "D3D";
+	// Set version of engine
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	// Set version of api
+	appInfo.apiVersion = VK_API_VERSION_1_0;
 }
 
 bool D3D::InstanceWrapper::CheckValidationLayerSupport(const std::vector<const char *> validationLayers)
 {
-	uint32_t layerCount;
+	// Create layercount
+	uint32_t layerCount{};
 
+	// Get layercount
 	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
+	// Create vector for layers the size of layercount
 	std::vector<VkLayerProperties> availableLayers(layerCount);
+	// Get the available layers
 	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
+	// Loop trough all the validation layers
 	for (const char* layerName : validationLayers)
 	{
+		// Create layerfound boolean
 		bool layerFound = false;
 
+		// Loop trough all the available layers
 		for (const auto& layerProperties : availableLayers)
 		{
+			// Compare the strings
 			if (strcmp(layerName, layerProperties.layerName) == 0)
 			{
+				// If the strings are equal, the layer has been found
 				layerFound = true;
+				// Stop the loop trough available layers
 				break;
 			}
 		}
 
+		// If the layer wasn't found, return false
 		if (!layerFound)
 			return false;
 	}
 
+	// If all layers were found, return true
 	return true;
 }
 
 std::vector<const char*> D3D::InstanceWrapper::GetRequiredExtensions(bool enableValidationLayers)
 {
+	// Crate uint fro the amount of glfw extensions
 	uint32_t glfwExtensionCount = 0;
+	// Create char** for the names of the glfw estensions
 	const char** glfwExtensions;
 
+	// Get the glfw extensions and their count
 	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
+	// Create a vector to hold the extensions, starting from the address of glfwExtensions and ending at glfwExtensions + the amount of extensions
 	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
+	// If the validation layers are enabled
 	if (enableValidationLayers)
 	{
+		// Add the debut utils extension
 		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 	}
 
+	// If the push descriptor is supported, add it to the extensions list
 	if (CheckExtensionSupport(VK_KHR_PUSH_DESCRIPTOR_EXTENSION_NAME))
 	{
 		extensions.push_back(VK_KHR_PUSH_DESCRIPTOR_EXTENSION_NAME);
 	}
 
+	// Return the extensions
 	return extensions;
 }
 
@@ -140,56 +193,73 @@ bool D3D::InstanceWrapper::CheckExtensionSupport(const char* extensionName)
 
 void D3D::InstanceWrapper::PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
 {
-	createInfo = {};
+	// Set type to messenger create info
 	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	// Only allow message severity of warning or error level
 	createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
 		| VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-	createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+	// Only allow message types of general, validation and performance
+	createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
 		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	// Give the debug callback function adress
 	createInfo.pfnUserCallback = debugCallback;
 }
 
 VKAPI_ATTR VkBool32 VKAPI_CALL D3D::InstanceWrapper::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT /*messageSeverity*/, VkDebugUtilsMessageTypeFlagsEXT /*messageType*/, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* /*pUserData*/)
 {
-	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+	// Print the validation layer error
+	std::cerr << "validation layer error: " << pCallbackData->pMessage << std::endl;
 
 	return VK_FALSE;
 }
 
 void D3D::InstanceWrapper::SetupDebugMessenger(bool enableValidationLayers)
 {
+	// If validation layers aren't enabled, return
 	if (!enableValidationLayers)
 		return;
 
+	// Create messenger create info
 	VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+	// Setup messenger crerate info
 	PopulateDebugMessengerCreateInfo(createInfo);
 
+	// Create the debugMessenger
 	if (CreateDebugUtilsMessengerEXT(m_Instance, &createInfo, nullptr, &m_DebugMessenger) != VK_SUCCESS)
 	{
+		// If unsuccessful, throw runtime error
 		throw std::runtime_error("failed to set up debug messenger!");
 	}
 }
 
 VkResult D3D::InstanceWrapper::CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
 {
+	// Get the adress of the debug messenger creation function
 	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
 
+	// If the function is still a nullptr, the extension is not present
 	if (func != nullptr)
 	{
+		// If the function is available, run it and return the result
 		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
 	}
 	else
 	{
+		// If the function is not present, return error
 		return VK_ERROR_EXTENSION_NOT_PRESENT;
 	}
 }
 
 void D3D::InstanceWrapper::DestroyDebugUtilsMessegerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator)
 {
+	// Get the adress of the debug messenger delete function
 	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
 
+	// If the function is still a nullptr, the extension is not present
 	if (func != nullptr)
 	{
+		// If the function is available, run it
 		func(instance, debugMessenger, pAllocator);
 	}
 }
--- source/VulkanRenderer3D/InstanceWrapper.h
+++ source/VulkanRenderer3D/InstanceWrapper.h
@@ -4,20 +4,27 @@
 #ifndef InstanceWrapperDefined
 #define InstanceWrapperDefined
 
+// File includes
 #include "VulkanIncludes.h"
 
+// Standard library includes
 #include <vector>
 
 namespace D3D
 {
-
 	class InstanceWrapper
 	{
 	public:
-		InstanceWrapper() = delete;
-
+		// Constructor
+		// Parameters:
+		//     enableValidationLayers: should validation layers be used or not
+		//     validationLayers: list of validation layers that should be used
 		InstanceWrapper(bool enableValidationLayers, const std::vector<const char*>& validationLayers);
 
+		// Delete default constructor
+		InstanceWrapper() = delete;
+
+		// Default destructor
 		~InstanceWrapper() = default;
 
 		InstanceWrapper(InstanceWrapper& other) = delete;
@@ -26,24 +33,52 @@ namespace D3D
 		InstanceWrapper& operator=(InstanceWrapper& other) = delete;
 		InstanceWrapper& operator=(InstanceWrapper&& other) = delete;
 
+		// Get the VkInstance
 		VkInstance GetInstance() const { return m_Instance; }
 
+		// Cleanup function
+		// Parameters:
+		//     enableValidationLayers: are validation layers enabled or not
 		void cleanup(bool enableValidationLayers);
 
 	private:
+		// Handle of the VkInstance
 		VkInstance m_Instance = VK_NULL_HANDLE;
 
+		// Handle for the debug messenger
 		VkDebugUtilsMessengerEXT m_DebugMessenger = VK_NULL_HANDLE;
 
+		// Set the values for the application info
+		// Parameters:
+		//     applicationInfo: a reference to the VkApplicationInfo struct to avoid making a new one in the function
+		void SetupApplicationInfo(VkApplicationInfo& appInfo);
 
+		// Helper function that checks if given validation layers are supported
+		// Parameters:
+		//     validationLayers: list of validation layers that should be checked
 		bool CheckValidationLayerSupport(const std::vector<const char*> validationLayers);
 
+		// Get the required extensions for the instance createion
+		// Parameters:
+		//     enableValidationLayers: are the validation layers enabled
 		std::vector<const char*> GetRequiredExtensions(bool enableValidationLayers);
 
+		// Check if a certain extension is supported
+		// Parameters:
+		//     extensionName: the name of the extension that should be checked
 		bool CheckExtensionSupport(const char* extensionName);
 
+		// Setup the debug messenger create info
+		// Parameters:
+		//     createInfo: a reference to the messenger create info to avoid creating a new one in the function
 		void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 
+		// Necesarry function for the callback of the debug messenger
+		// Parameters:
+		//     messageSeverity: the severity of the current message
+		//     messageType: the type of the current message
+		//     pCallbackData: the pointer that contains the message
+		//     pUserData: data of current user
 		static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 			VkDebugUtilsMessageTypeFlagsEXT messageType,
@@ -51,11 +86,28 @@ namespace D3D
 			void* pUserData
 		);
 
+		// Set up the debug messenger
+		// Parameters:
+		//     enableValidationLayers: are the validation layers enabled
 		void SetupDebugMessenger(bool enableValidationLayers);
 
-		VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+		// Create the debug messenger
+		// Parameters:
+		//     instance: handle of the VKInstance
+		//     pCreateInfo: pointer to the messenger create info
+		//     pAllocator: pointer for callbacks
+		//     pDebugMessenger: pointer to the debug messenger
+		VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+			const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator,
+			VkDebugUtilsMessengerEXT* pDebugMessenger);
 	
-		void DestroyDebugUtilsMessegerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+		// Destroy the debug messenger
+		// Parameters:
+		//     instance: handle of the VKInstance
+		//     pDebugMessenger: pointer to the debug messenger
+		//     pAllocator: pointer for callbacks
+		void DestroyDebugUtilsMessegerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger,
+			const VkAllocationCallbacks* pAllocator);
 	};
 
 }
