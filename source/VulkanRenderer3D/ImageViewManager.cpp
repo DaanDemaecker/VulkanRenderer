e29999bd65c28d5d02d01067702ff8c036070445
Daan Demaecker <daan.demaecker@gmail.com>
2024-03-03 15:13:39 +0100

    3D: Added commentary to the imageviewmanager class

--- source/VulkanRenderer3D/ImageViewManager.cpp
+++ source/VulkanRenderer3D/ImageViewManager.cpp
@@ -1,38 +1,53 @@
+// ImageViewManager.cpp
+
+// File includes
 #include "ImageViewManager.h"
 #include "ImageManager.h"
 
 
 D3D::ImageViewManager::ImageViewManager(VkSampleCountFlagBits msaaSamples)
+	// Initialize max amount of samples per pixel
 	:m_MsaaSamples{msaaSamples}
 {
 }
 
 void D3D::ImageViewManager::Cleanup(VkDevice device)
 {
+	// Clean up color image
 	m_ColorImage.Cleanup(device);
+	// Clean up depth image
 	m_DepthImage.Cleanup(device);
 }
 
 void D3D::ImageViewManager::CreateColorResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat format, VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager)
 {
-	VkFormat colorFormat = format;
-
-	pImageManager->CreateImage(device, physicalDevice, swapchainExtent.width, swapchainExtent.height, 1, m_MsaaSamples, colorFormat,
+	// Create the image for the color
+	// Set tiling to optimal
+	// Set usage to transient attachment and color attachment
+	// Set properties to device local
+	pImageManager->CreateImage(device, physicalDevice, swapchainExtent.width, swapchainExtent.height, 1, m_MsaaSamples, format,
 		VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_ColorImage);
 
-	m_ColorImage.imageView = pImageManager->CreateImageView(device, m_ColorImage.image, colorFormat, VK_IMAGE_ASPECT_COLOR_BIT, 1);
+	// Create image view for the color image
+	m_ColorImage.imageView = pImageManager->CreateImageView(device, m_ColorImage.image, format, VK_IMAGE_ASPECT_COLOR_BIT, 1);
 }
 
 void D3D::ImageViewManager::CreateDepthResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat depthFormat, VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager, VkCommandBuffer commandBuffer)
 {
+	// Create the image for the depth
+	// Set tiling to optimal
+	// Set usage to depth stencil attachment bit
+	// Set properties to device local
 	pImageManager->CreateImage(device, physicalDevice, swapchainExtent.width, swapchainExtent.height, 1, m_MsaaSamples, depthFormat,
 		VK_IMAGE_TILING_OPTIMAL,
 		VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 		m_DepthImage);
 
+	// Create image view for the depth image
 	m_DepthImage.imageView = pImageManager->CreateImageView(device, m_DepthImage.image, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT, 1);
 
+	// Transition the depth image from undefined to depth stencil attachment optimal
 	pImageManager->TransitionImageLayout(m_DepthImage.image, commandBuffer, depthFormat, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL, 1);
 }
--- source/VulkanRenderer3D/ImageViewManager.h
+++ source/VulkanRenderer3D/ImageViewManager.h
@@ -7,12 +7,21 @@
 
 namespace D3D
 {
+	// Class forward declaration for image manager
 	class ImageManager;
 
 	class ImageViewManager
 	{
 	public:
+		// Constructor
+		// Parameters:
+		//     msaaSamples: max amount of samples per pixel
 		ImageViewManager(VkSampleCountFlagBits msaaSamples);
+
+		// Delete default constructor
+		ImageViewManager() = delete;
+
+		// Dafault destructor
 		~ImageViewManager() = default;
 
 		ImageViewManager(ImageViewManager& other) = delete;
@@ -21,21 +30,45 @@ namespace D3D
 		ImageViewManager& operator=(ImageViewManager& other) = delete;
 		ImageViewManager& operator=(ImageViewManager&& other) = delete;
 
+		// Clean up allocated objects
+		// Parameters:
+		//     device: handle to the VkDevice
 		void Cleanup(VkDevice device);
 
-		void CreateColorResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat format, VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager);
-		void CreateDepthResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat depthFormat, VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager, VkCommandBuffer commandBuffer);
+		// Initialize the color image for the swapchain
+		// Parameters:
+		//     device: handle of the VkDevice
+		//     physicalDevice: handle of the VkPhysicalDevice
+		//     format: the requested format for the color image
+		//     swapchainExtent: the extent of the swapchain, for use of the image size
+		//     pImageManager: pointer to the image manager
+		void CreateColorResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat format,
+			VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager);
+		// Initialize the depth image for the swapchain
+		// Parameters:
+		//     device: handle of the VkDevice
+		//     physicalDevice: handle of the VkPhysicalDevice
+		//     depthFormat: the requested format for the depth image
+		//     swapchainExtent: the extent of the swapchain, for use of the image size
+		//     pImageManager: pointer to the image manager
+		void CreateDepthResources(VkDevice device, VkPhysicalDevice physicalDevice, VkFormat depthFormat,
+			VkExtent2D swapchainExtent, D3D::ImageManager* pImageManager, VkCommandBuffer commandBuffer);
 
+		// Get the imageView for the color image
 		VkImageView GetColorImageView() const { return m_ColorImage.imageView; }
+		// Get the imageView for the depth image
 		VkImageView GetDepthImageView() const { return m_DepthImage.imageView; }
+		// Get the max amount of samples per pixel
 		VkSampleCountFlagBits GetMsaaSamples() const { return m_MsaaSamples; }
 
 	private:
-		//--MultiSampling--
+		// Max amount of samples per pixel, initialize as 1
 		VkSampleCountFlagBits m_MsaaSamples = VK_SAMPLE_COUNT_1_BIT;
+
+		// Texture struct for the color image
 		Texture m_ColorImage{};
 
-		//--Depth Image--
+		// Texture struct for the depth image
 		Texture m_DepthImage{};
 	};
 }
