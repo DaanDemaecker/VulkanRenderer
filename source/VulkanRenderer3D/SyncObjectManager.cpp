ea1acd2dcadb8fc483c5bc2540599b75f644639d
Daan Demaecker <daan.demaecker@gmail.com>
2024-03-04 16:13:53 +0100

    3D: Added commentary to sync object manager

--- source/VulkanRenderer3D/SyncObjectManager.cpp
+++ source/VulkanRenderer3D/SyncObjectManager.cpp
@@ -1,44 +1,61 @@
+// SyncObjectManager.cpp
 
+// File includes
 #include "SyncObjectManager.h"
 
+// Standard library includes
 #include <stdexcept>
 
 D3D::SyncObjectManager::SyncObjectManager(VkDevice device, uint32_t maxFrames)
 {
+	// Initialize the sync objects
 	CreateSyncObjects(device, maxFrames);
 }
 
 void D3D::SyncObjectManager::Cleanup(VkDevice device)
 {
+	// Loop trough the amount of fences and semaphores
 	for (size_t i = 0; i < m_ImageAvailableSemaphores.size(); ++i)
 	{
+		// Destroy image available semaphore
 		vkDestroySemaphore(device, m_ImageAvailableSemaphores[i], nullptr);
+		// Destroy render finished semaphore
 		vkDestroySemaphore(device, m_RenderFinishedSemaphores[i], nullptr);
+		// Destroy in flight fence
 		vkDestroyFence(device, m_InFlightFences[i], nullptr);
 	}
 }
 
 void D3D::SyncObjectManager::CreateSyncObjects(VkDevice device, uint32_t maxFrames)
 {
+	// Resize image available semaphore to the amound of frames
 	m_ImageAvailableSemaphores.resize(maxFrames);
+	// Resize render finished semaphore to the amount of frames
 	m_RenderFinishedSemaphores.resize(maxFrames);
+	// Resize the in fligh fences to the amount of frames
 	m_InFlightFences.resize(maxFrames);
 
-
+	// Create semaphore create info
 	VkSemaphoreCreateInfo semaphoreInfo{};
+	// Set type to semaphore create info
 	semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
+	// Create fence create info
 	VkFenceCreateInfo fenceInfo{};
+	// Set type to fence create info
 	fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+	// Set flags to signaled
 	fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
-
+	// Loop trough the amound of frames
 	for (size_t i = 0; i < maxFrames; ++i)
 	{
+		// Create the sync objects
 		if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &m_ImageAvailableSemaphores[i]) != VK_SUCCESS ||
 			vkCreateSemaphore(device, &semaphoreInfo, nullptr, &m_RenderFinishedSemaphores[i]) != VK_SUCCESS ||
 			vkCreateFence(device, &fenceInfo, nullptr, &m_InFlightFences[i]) != VK_SUCCESS)
 		{
+			// If unsuccesful, throw runtime error
 			throw std::runtime_error("failed to create sync objects!");
 		}
 	}
--- source/VulkanRenderer3D/SyncObjectManager.h
+++ source/VulkanRenderer3D/SyncObjectManager.h
@@ -15,7 +15,16 @@ namespace D3D
 	class SyncObjectManager final
 	{
 	public:
+		// Delete default constructor
+		SyncObjectManager() = delete;
+
+		// Constructor
+		// Parameters:
+		//     device: handle of the VkDevice
+		//     maxFrames: the maximum amount of frames in flight
 		SyncObjectManager(VkDevice device, uint32_t maxFrames);
+
+		// Default destructor
 		~SyncObjectManager() = default;
 
 		SyncObjectManager(SyncObjectManager& other) = delete;
@@ -24,19 +33,39 @@ namespace D3D
 		SyncObjectManager& operator=(SyncObjectManager& other) = delete;
 		SyncObjectManager& operator=(SyncObjectManager&& other) = delete;
 
+		// Clean up allocated objects
 		void Cleanup(VkDevice device);
 
+		// Get the requested image available semaphore
+		// Parameters:
+		//     frame: current frame and index of the semaphore
 		VkSemaphore& GetImageAvailableSemaphore(uint32_t frame) { return m_ImageAvailableSemaphores[frame]; }
+
+		// Get the requested render finished semaphore
+		// Parameters:
+		//     frame: current frame and index of the semaphore
 		VkSemaphore& GetRenderFinishedSemaphore(uint32_t frame) { return m_RenderFinishedSemaphores[frame]; }
+
+		// Get the requested in flight fence
+		// Parameters:
+		//     frame: current frame and index of the fence
 		VkFence& GetInFlightFence(uint32_t frame) { return m_InFlightFences[frame]; }
 
 	private:
 
-		//--Sync objects--
+		// Vector of image available semaphores
 		std::vector<VkSemaphore> m_ImageAvailableSemaphores{};
+
+		// Vector of render finished semaphores
 		std::vector<VkSemaphore> m_RenderFinishedSemaphores{};
+
+		// Vector of in flight fences
 		std::vector<VkFence> m_InFlightFences{};
 
+		// Initialize the sync objects
+		// Paramters:
+		//     device: handle of the VkDevice
+		//     maxFrames: the max amount of frames in fligh
 		void CreateSyncObjects(VkDevice device, uint32_t maxFrames);
 	};
 }
