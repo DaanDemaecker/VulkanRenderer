24a84ad97715d1219ce2dc802b0ae8d4a3796074
Daan Demaecker <daan.demaecker@gmail.com>
2024-03-10 10:30:12 +0100

    2D: Added singleton and VulkanRenderer2D classes

---
+++ source/VulkanRenderer2D/Singleton.h
@@ -0,0 +1,35 @@
+// Singleton.h
+// This class will serve as the base for any singleton
+
+#ifndef SingletonIncluded
+#define SingletonIncluded
+
+namespace D2D
+{
+	// Make singletion templated class
+	template <typename T>
+	class Singleton
+	{
+	public:
+		// Get the single instance of the class
+		static T& GetInstance()
+		{
+			// Create static instance, because the variable is static, this will only be done once
+			static T instance{};
+			// Return the instance
+			return instance;
+		}
+
+		virtual ~Singleton() = default;
+		Singleton(const Singleton& other) = delete;
+		Singleton(Singleton&& other) = delete;
+		Singleton& operator=(const Singleton& other) = delete;
+		Singleton& operator=(Singleton&& other) = delete;
+
+	protected:
+		// Make constructor protected so a second instance can't be made from outside the class
+		Singleton() = default;
+	};
+}
+
+#endif // !SingletonIncluded
---
+++ source/VulkanRenderer2D/VulkanRenderer2D.h
@@ -0,0 +1,24 @@
+// VulkanRenderer2D
+// This file will act as the main renderer, it holds all the objects needed for rendering
+
+#ifndef VulkanRenderer2DIncluded
+#define VulkanRenderer2DIncluded
+
+// File includes
+#include "Singleton.h"
+
+
+
+namespace D2D
+{
+	class VulkanRenderer2D : public Singleton<VulkanRenderer2D>
+	{
+	public:
+
+	private:
+
+	};
+}
+
+
+#endif // !VulkanRenderer2DIncluded
