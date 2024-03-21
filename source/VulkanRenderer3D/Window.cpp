01da8dbd02755cae5c26770aad0974f9e6b3e867
Daan Demaecker <daan.demaecker@gmail.com>
2024-03-21 21:13:21 +0100

    3D: Added commentary in configuratoin manager and window creation function

--- source/VulkanRenderer3D/Window.cpp
+++ source/VulkanRenderer3D/Window.cpp
@@ -35,16 +35,69 @@ void D3D::Window::SetFrameBufferResized(bool value)
 
 void D3D::Window::InitWindow()
 {
+	// Initialize glfw
 	glfwInit();
 
 	//Tell GLFW not to create an OpenGL context
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
+	// Get a reference to the configmanager for later use
+	auto& configManager{ ConfigManager::GetInstance() };
 
+	// Get info ffrom config manager
+	bool fullScreen{configManager.GetBool("FullScreen")};
+	int monitorIndex{ configManager.GetInt("Monitor") };
+
+	// Get a list of all the available monitors
+	int count{};
+	GLFWmonitor** monitors{ glfwGetMonitors(&count) };
+
+	// Create monitor variable and initialize with nullptr
+	// If fullscreen is enabled, this will hold the correct monitor
+	// If fullscreen is not enabled, this will stay nullptr
+	GLFWmonitor* monitor{ nullptr };
+
+	if (fullScreen)
+	{
+		// If the monitor index is available, set monitor, if not, get primary monitor
+		if (monitorIndex < count)
+		{
+			monitor = monitors[monitorIndex];
+		}
+		else
+		{
+			monitor = glfwGetPrimaryMonitor();
+		}
+	}
 
 	//Initialize the window
-	m_Window.pWindow = glfwCreateWindow(m_Window.Width, m_Window.Height, ConfigManager::GetInstance().GetString("WindowName").c_str(),
-		nullptr, nullptr);
+	m_Window.pWindow = glfwCreateWindow(m_Window.Width, m_Window.Height, configManager.GetString("WindowName").c_str(),
+		monitor, nullptr);
+
+
+	// If fullscreen is disabled, the monitor index is available and isn't 0, calculate position for the window on the requested monitor
+	if (!fullScreen && monitorIndex != 0 && monitorIndex < count)
+	{
+		int x, y, w, h;
+
+		// Get the current monitor
+		auto currentMonitor{ monitors[monitorIndex] };
+
+		// Get all the measurements of the monitor
+		glfwGetMonitorWorkarea(currentMonitor, &x, &y, &w, &h);
+		
+		// Calculate new position for the screen
+		int newX{ x + (w/2) - (m_Window.Width/2)};
+		int newY{ y + (h/2) - (m_Window.Width/2)};
+
+		// Get refreshrate
+		auto refreshRate{glfwGetVideoMode(currentMonitor)->refreshRate};
+
+		// Set the new window position
+		glfwSetWindowMonitor(m_Window.pWindow, nullptr, newX, newY, m_Window.Width, m_Window.Height, refreshRate);
+	}
+
+	// Set window user pointer and callbacks
 	glfwSetWindowUserPointer(m_Window.pWindow, this);
 	glfwSetFramebufferSizeCallback(m_Window.pWindow, FramebufferResizeCallback);
 }
