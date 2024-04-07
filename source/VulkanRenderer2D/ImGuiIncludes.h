9a1449df24651f696681e7fc6e833e5982c18ca0
Daan Demaecker <daan.demaecker@gmail.com>
2024-04-07 21:36:50 +0100

    2D: Created ImGuiIncludes.h file

---
+++ source/VulkanRenderer2D/ImGuiIncludes.h
@@ -0,0 +1,10 @@
+// ImGuiIncludes.h
+
+#ifndef ImGuiIncluded
+#define ImGuiIncluded
+
+#include <imgui.h>
+#include <backends/imgui_impl_vulkan.h>
+#include <backends/imgui_impl_glfw.h>
+
+#endif // !ImGuiIncluded
