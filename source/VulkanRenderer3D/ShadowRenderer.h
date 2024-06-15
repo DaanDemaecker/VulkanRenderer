59192ad1253a33a2ec5da47cc3300b7b125380c7
Daan Demaecker <daan.demaecker@gmail.com>
2024-06-15 21:08:25 +0200

    3D: Created ShadowRenderer class

---
+++ source/VulkanRenderer3D/ShadowRenderer.h
@@ -0,0 +1,20 @@
+#ifndef ShadowRendererIncluded
+#define ShadowRendererIncluded
+
+// File includes
+#include "VulkanIncludes.h"
+
+// Standard library includes
+
+namespace D3D
+{
+	class ShadowRenderer final
+	{
+	public:
+
+	private:
+
+	};
+}
+
+#endif // !ShadowRendererIncluded
