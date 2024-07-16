20fe20d604d10c3030ca6660ad4aab6429b729be
Daan Demaecker <daan.demaecker@gmail.com>
2024-07-16 21:27:30 +0200

    2D: if more than 0 physical devices is found, one is assigned to member variable

--- VulkanRenderer/VulkanRenderer2D/Vulkan/Wrappers/GPUObject.cpp
+++ VulkanRenderer/VulkanRenderer2D/Vulkan/Wrappers/GPUObject.cpp
@@ -4,6 +4,7 @@
 #include "GPUObject.h"
 
 // Standard library includes
+#include <stdexcept>
 
 D2D::GPUObject::GPUObject(const VkInstance& instance)
 {
@@ -15,6 +16,16 @@ void D2D::GPUObject::SetupPhysicalDevice(const VkInstance& instance)
 	std::vector<VkPhysicalDevice> physicalDevices{};
 
 	EnumeratePhysicalDevices(instance, physicalDevices);
+
+
+	if (physicalDevices.size() == 0)
+	{
+		throw std::runtime_error("Failed to find suitable physical device");
+	}
+	else
+	{
+		m_PhysicalDevice = physicalDevices[0];
+	}
 }
 
 void D2D::GPUObject::EnumeratePhysicalDevices(const VkInstance& instance, std::vector<VkPhysicalDevice>& devices)
