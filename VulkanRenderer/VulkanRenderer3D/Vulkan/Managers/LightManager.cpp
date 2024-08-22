1e763a49b3618c215274bb0a15bf25e9b2d6ac09
Daan Demaecker <daan.demaecker@gmail.com>
2024-08-22 21:26:22 +0200

    3D: Created light manager class

---
+++ VulkanRenderer/VulkanRenderer3D/Vulkan/Managers/LightManager.cpp
@@ -0,0 +1,12 @@
+// LightManager.cpp
+
+// Header include
+#include "LightManager.h"
+
+D3D::LightManager::LightManager()
+{
+}
+
+D3D::LightManager::~LightManager()
+{
+}
---
+++ VulkanRenderer/VulkanRenderer3D/Vulkan/Managers/LightManager.h
@@ -0,0 +1,21 @@
+// LightManager.h
+// This class will hold and handle all lights in the scene
+
+#ifndef LightManagerIncluded
+#define LightManagerIncluded
+
+namespace D3D
+{
+	class LightManager
+	{
+	public:
+		LightManager();
+
+		~LightManager();
+
+	private:
+
+	};
+}
+
+#endif // !LightManagerIncluded
