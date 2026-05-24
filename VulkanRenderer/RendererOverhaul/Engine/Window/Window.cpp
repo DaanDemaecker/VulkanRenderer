7c22d901fa19a2794ef980e8637db32c918b321c
Daan Demaecker <daan.demaecker@gmail.com>
2026-05-24 23:00:28 +0200

    Window is no longer resizeable

--- VulkanRenderer/RendererOverhaul/Engine/Window/Window.cpp
+++ VulkanRenderer/RendererOverhaul/Engine/Window/Window.cpp
@@ -59,6 +59,8 @@ namespace DDM
 
 			InitData initData = ReadInitData();
 
+			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
 			auto handle = glfwCreateWindow(initData.width, initData.height, ConfigManager::GetInstance().GetString("WindowName"), nullptr, nullptr);
 
 
