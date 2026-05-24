f04c5dc112f3862b1d797c1b8746ed07693d2397
Daan Demaecker <daan.demaecker@gmail.com>
2026-05-24 11:54:55 +0200

    Improved CMakeSettings.json, updated glfw version, only include vld if in debug mode

--- VulkanRenderer/RendererOverhaul/Includes/GLFWIncludes.h
+++ VulkanRenderer/RendererOverhaul/Includes/GLFWIncludes.h
@@ -3,6 +3,6 @@
 #ifndef _DDM_GLFW_
 #define _DDM_GLFW_ 
 
-#include <GLFW/glfw3.h>
+#include "GLFW/glfw3.h"
 
 #endif // !_DDM_GLFW_
--- VulkanRenderer/RendererOverhaul/Includes/VldIncludes.h
+++ VulkanRenderer/RendererOverhaul/Includes/VldIncludes.h
@@ -1,9 +1,16 @@
 // VldIncludes.h
 // Include visual leak detector
 
+#if DEBUG
+
 #ifndef _DDM_VLD_INCLUDES_
 #define _DDM_VLD_INCLUDES_
 
 #include "vld.h"
 
 #endif // !_DDM_VLD_INCLUDES_
+
+#endif // DEBUG
+
+
+
